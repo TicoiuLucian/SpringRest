ba954e067a88911bf1ec965598d90877188f0a30
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2022-12-07 22:30:18 +0200

    insert data into database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO car ( manufacturer, model, car_year) VALUES ('Renault', 'Symbol', '2009');
+INSERT INTO car (manufacturer, model, car_year) VALUES ('Ford', 'Mondeo', '2009');
+INSERT INTO car (manufacturer, model, car_year) VALUES ('Toyota', 'Corolla', '2019');
+INSERT INTO car (manufacturer, model, car_year) VALUES ('Hyundai', 'I30', '2022');
+INSERT INTO car (manufacturer, model, car_year) VALUES ('Vw', 'Tiguan', '2019');
+INSERT INTO car (manufacturer, model, car_year) VALUES ('Toyota', 'Rav4', '2017');
+INSERT INTO car (manufacturer, model, car_year) VALUES ('Opel', 'Astra', '2019');
+INSERT INTO car (manufacturer, model, car_year) VALUES ('Vw', 'Jetta', '2017');
+
+INSERT INTO owner(name,Fk_Car_Id) VALUES ('Popescu',1);
+INSERT INTO owner(name,Fk_Car_Id) VALUES ('Ionescu',2);
+INSERT INTO owner(name,Fk_Car_Id) VALUES ('Ionescu',3);
+INSERT INTO owner(name,Fk_Car_Id) VALUES ('Ionescu',5);
+
+
+
