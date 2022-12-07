05d63a7b71b7f9ec8f641090954582f725d5b41a
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2022-12-07 23:09:51 +0200

    fixed schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,20 @@
+CREATE TABLE IF NOT EXISTS Owner
+(
+    Pk_Owner_Id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100)
+);
+
 CREATE TABLE IF NOT EXISTS Car
 (
     Pk_Car_Id INT PRIMARY KEY AUTO_INCREMENT,
     manufacturer VARCHAR(100),
     model VARCHAR(100),
-    car_year VARCHAR(100)
+    car_year VARCHAR(100),
+    Fk_Owner_Id INT,
+    FOREIGN KEY(Fk_Owner_Id) REFERENCES Owner(Pk_Owner_Id)
 );
 
-CREATE TABLE IF NOT EXISTS Owner
-(
-    Pk_Owner_Id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
-    Fk_Car_Id INT,
-    FOREIGN KEY(Fk_Car_Id) REFERENCES Car(Pk_Car_Id)
-);
+
 
 
 
